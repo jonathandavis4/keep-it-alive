6fbe1b1edcb325d0bc5a3a490f18101045b7d086
Jonathan Davis <jonathandavis47780@gmail.com>
2020-04-18 13:53:42 +0100

    Turn the fish to face the direction they are going.

--- js/fish.js
+++ js/fish.js
@@ -1,25 +1,25 @@
 class Fish {
     constructor() {
-        this.canvas = document.createElement('canvas');
-        this.canvas.width = 20;
-        this.canvas.height = 20;
-        this.context = this.canvas.getContext('2d');
+        this.left_canvas = document.createElement('canvas');
+        this.left_canvas.width = 20;
+        this.left_canvas.height = 20;
+        this.left_canvas_context = this.left_canvas.getContext('2d');
 
         // Prepare the image.
-        for (let j = 0; j < this.canvas.height; j++) {
-            for (let i = 0; i < this.canvas.width; i++) {
+        for (let j = 0; j < this.left_canvas.height; j++) {
+            for (let i = 0; i < this.left_canvas.width; i++) {
                 let index = (20 * j) + i;
                 let r = image_fish_1[index][0];
                 let g = image_fish_1[index][1];
                 let b = image_fish_1[index][2];
 
                 if (r == 0 && g == 255 && b == 0) {
-                    this.context.fillStyle = 'rgba(0, 0, 0, 0)';
-                    this.context.fillRect(i, j, 1, 1)
+                    this.left_canvas_context.fillStyle = 'rgba(0, 0, 0, 0)';
+                    this.left_canvas_context.fillRect(i, j, 1, 1)
                 }
                 else {
-                    this.context.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-                    this.context.fillRect(i, j, 1, 1);              
+                    this.left_canvas_context.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+                    this.left_canvas_context.fillRect(i, j, 1, 1);              
                 }
             }
         }
@@ -33,6 +33,15 @@ class Fish {
         this.max_y_velocity = 5;
         this.x_velocity = random(this.min_x_velocity, this.max_x_velocity);
         this.y_velocity = random(this.min_y_velocity, this.max_y_velocity);
+
+        // Generate the mirrored image.
+        this.right_canvas = document.createElement('canvas');
+        this.right_canvas.width = 20;
+        this.right_canvas.height = 20;
+        this.right_canvas_context = this.right_canvas.getContext('2d');
+        this.right_canvas_context.scale(-1, 1);
+        this.right_canvas_context.translate(-this.right_canvas.width, 0)
+        this.right_canvas_context.drawImage(this.left_canvas, 0, 0, this.right_canvas.width, this.right_canvas.height);
     }
 
     logic() {
@@ -79,6 +88,11 @@ class Fish {
     }
 
     draw() {
-        draw_image(this.canvas, this.canvas.width, this.canvas.height, this.x, this.y, 2);
+        if (this.x_velocity > 0) {
+            draw_image(this.right_canvas, this.right_canvas.width, this.right_canvas.height, this.x, this.y, 2);
+        }
+        else {
+            draw_image(this.left_canvas, this.left_canvas.width, this.left_canvas.height, this.x, this.y, 2);
+        }
     }
 }
